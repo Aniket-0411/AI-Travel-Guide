f697c72248935c738200960376896b4da07f2693
evolentDelight <evolentDelight@outlook.com>
2024-05-05 18:22:04 -0700

    Add Chat Page

--- cmpe280-project/src/App.js
+++ cmpe280-project/src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./Pages/Navigation";
 import MainPage from "./Pages/MainPage";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import ChatAI from "./Pages/ChatAI"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <div>
           <Routes>
             <Route path="/" exact element={<MainPage/>}/>
+            <Route path="/chat" exact element ={<ChatAI/>}/>
             <Route path="/login" exact element={<Login/>}/>
             <Route path="/register" exact element={<Register/>}/>
           </Routes>
